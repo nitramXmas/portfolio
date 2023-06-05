7e466005ccdcdac8a87b34b3c2ba2e6681cf92c2
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-06-05 23:06:58 +0200

    positionning structure elements in mobile first way

---
+++ client/src/components/Experiences.jsx
@@ -0,0 +1,14 @@
+import './experiences.css'
+
+const Experiences = () => {
+  return (
+    <div className='experiences'>
+        <h4>Experience</h4>
+        <div className="timeline">
+            <div className='line'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Experiences
